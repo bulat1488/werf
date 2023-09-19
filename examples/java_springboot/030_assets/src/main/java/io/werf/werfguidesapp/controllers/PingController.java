c49e00c4a5990fcd74af97e95a7abd8fc686141b
Ваше Имя <you@example.com>
2023-09-19 18:33:51 +0300

    first commit

---
+++ examples/java_springboot/030_assets/src/main/java/io/werf/werfguidesapp/controllers/PingController.java
@@ -0,0 +1,14 @@
+package io.werf.werfguidesapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class PingController {
+
+	@GetMapping("/ping")
+	public String Ping() {
+		return "pong\n";
+	}
+}
